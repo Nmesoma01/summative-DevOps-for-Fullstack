e686f0282d11063268dfc19fc5a5599eb420d276
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2024-07-31 19:50:34 +0200

    lint error

--- nodejs_app/app.js
+++ nodejs_app/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00e62aa247127df9292f9ca0591f56235f52169f4d756b469a2d82b6739c9dbf
-size 1130
+oid sha256:7dda74d2a845e052e6d2a0c31bc6a5ed579f13df5eed074a99d919aa5310dc01
+size 1186
